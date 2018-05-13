192cbc1873210d1fa84f06a4cc57a1c3b6af9268
LucaPaterlini <LucaPaterlini@github.com>
2018-05-13 14:15:34 +0100

    minor bug fixes masternode.h

--- src/masternode.h
+++ src/masternode.h
@@ -24,7 +24,7 @@ static const int MASTERNODE_NEW_START_REQUIRED_SECONDS  = 180 * 60;
 
 static const int MASTERNODE_POSE_BAN_MAX_SCORE          = 5;
 
-static const string DEVADDRESS("CWLef6dVQnnEwMuPJfyRghF1QZUb5d2A3f");
+static const string DEVADDRESS = "CWLef6dVQnnEwMuPJfyRghF1QZUb5d2A3f";
 //
 // The Masternode Ping Class : Contains a different serialize method for sending pings from masternodes throughout the network
 //
